7355052682972b7049d04a041fe98b5183cc9788
Alireza Prf <33633327+alirezaprf@users.noreply.github.com>
2020-03-29 17:48:17 +0430

    Update Othelo.java

--- Othelo.java
+++ Othelo.java
@@ -7,13 +7,72 @@ import java.awt.event.ActionListener;
 public class Othelo {
 
     private JFrame frame;
+    private int[][] gameBoard;
+    private boolean playerTurn=true;
+    //if true black moves else white does
+    private ActionListener ButtonsAction;
+
+
+
+
+    /**
+     * gameBorad Containg Game's Data as int
+     * 0 which is the initial State of every cell = empty
+     * 1 means black
+     * 2 means white 
+     */
 
     public Othelo() {
+        gameBoard=new int[8][8];
+        
+        gameBoard[3][3]=2;
+        gameBoard[4][4]=2;
+        
+        gameBoard[3][4]=1;
+        gameBoard[4][3]=1;
+
+        ButtonsAction=Set_Button_Actions();
+        
+
         frame = new JFrame("Othelo");
         SetProperties();
 
     }
+    
+    private ActionListener Set_Button_Actions() {
+        return new ActionListener() {
 
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println(e.getActionCommand());
+                Integer cmd=new Integer(e.getActionCommand());
+                int i=cmd.intValue()/8;
+                int j=cmd.intValue()%8;
+                gameBoard[i][j]=playerTurn?1:2;
+                printBoard();
+
+                playerTurn=!playerTurn;
+
+            }
+
+        };
+    }
+
+    private void printBoard()
+    {
+        //Debug
+        for (int i = 0; i < gameBoard.length; i++) {
+            for (int j = 0; j < gameBoard.length; j++) {
+                char p=gameBoard[i][j]==1?'☻':' ';
+                p=gameBoard[i][j]==2?'☺':p;
+                System.out.print(p +" | ");
+            }
+            System.out.println("\n--------------------------------");
+        }
+        //Debug
+        
+    }
+    
     public void SetProperties() {
         frame.setSize(800, 800);
         Draw();
@@ -29,17 +88,9 @@ public class Othelo {
             b.setVisible(true);
             b.setBackground(Color.GRAY);
             b.setForeground(b.getBackground());
-            b.setActionCommand(Integer.toString(i+1));
+            b.setActionCommand(Integer.toString(i));
             
-            b.addActionListener(new ActionListener() {
-
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.out.println(e);
-
-                }
-
-            });
+            b.addActionListener(ButtonsAction);
             frame.add(b);
         }
     }
